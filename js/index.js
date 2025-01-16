28db5a09c3cf47868e670fbf52eda0f208953cec
Rinel <rineliniguezsosa@gmail.com>
2025-01-16 14:38:09 -0600

    [FEAT] Cambios en el archivo index.js
    - convirtiendo el método getipaddress en asincrono
    - creamos un bloque if primero para validar que si existe algun valor en ipaddress pasaremos a consumir la api
    - creamos un bloque try catch para verificar la petición
    - creé la variable API_KEY para almacenar el string de mi apikey de geo.ipify
    - creé la variable req en donde esperé la respuesta http mediante fetch en donde consultamos mediante el valor de la ipaddress
    - imprimi en consola el valor de request ok
    - creé un bloque if para validar el status de la respuesta del server y si es tru creé una variable info en donde esperé la transformación de la data en formato json manipulable, estableci logs para ver el contenido de respuesta
    - tarde un poco ya que estaba intentando usar dotenv para que mi api key no se mostrará pero observe que era más relajo

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 
 //methods
-const getipaddress = () =>{
+const getipaddress = async() =>{
     const ipadress = document.getElementById('ipaddress').value
     console.log("valor",ipadress);
+    if (ipadress) {
+        try {
+            const API_KEY = "at_ni029uBQxjPqAHdYFZ41lO7T2nANz";
+            const req = await fetch(`https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ipadress}`);
+            console.log("valor de request ok:",req.ok);
+            if(req.ok){
+                const info = await req.json()
+                console.log("info api",info);
+                
+            }
+            console.log(resp);
+            
+        } catch (error) {
+            
+        }
+    }
 
 }
 
