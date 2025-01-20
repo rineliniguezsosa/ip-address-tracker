4839e46d769705bf67bfd276983e53cf4cc88581
Rinel <rineliniguezsosa@gmail.com>
2025-01-19 19:30:26 -0600

    [FEAT] Multiples cambios en el contenido de detailip, guardando los cambios para el diseño responsive del break point lg justo a los 1024px

--- js/index.js
+++ js/index.js
@@ -30,31 +30,57 @@ const renderIpDetails = (data) =>{
     const detailip  = `
         <div
           class="w-4/5 h-4/5 py-3 px-3 bg-white absolute top-[61%] rounded-lg border-3
-          border-blue-400 flex flex-col z-10"
+          flex flex-col z-10 lg:flex-row py-2 px-6 items-center h-[60%]"
         >
-          <div class="w-full h-1/4 pb-2 text-center">
-            <h2 class="font-rubik font-semibold text-darkgray text-[0.6rem]">IP ADDRESS</h2>
-            <p class="font-rubik font-semibold text-blackdarkgray text-lg">
-              ${data.ip}
-            </p>
+          <div class="w-full h-1/4 py-2 text-center lg:h-1/2 px-5 flex flex-col items-start 
+         border-r-2 border-darkgray-500">
+
+            <div>
+              <h2 class="font-rubik font-semibold text-darkgray text-[0.6rem]">IP ADDRESS</h2>
+            </div>
+
+            <div>
+              <p class="font-rubik font-semibold text-blackdarkgray text-lg">
+                ${data.ip}
+              </p>
+            </div>
+
           </div>
-          <div class="w-full h-1/4 py-2 text-center">
-            <h2 class="font-rubik font-semibold text-darkgray text-[0.6rem]">LOCATION</h2>
-            <p class="font-rubik font-semibold text-blackdarkgray text-lg">
-              ${data.location.region},${data.location.city} ${data.location.postalCode}
-            </p>
+
+          <div class="w-full h-1/4 py-2 text-center lg:h-1/2 px-5 flex flex-col items-start border-r-2 border-darkgray-500">
+
+            <div>
+              <h2 class="font-rubik font-semibold text-darkgray text-[0.6rem]">LOCATION</h2>
+            </div>
+
+            <div>
+              <p class="font-rubik font-semibold text-blackdarkgray text-lg">
+                ${data.location.region},${data.location.city} ${data.location.postalCode}
+              </p>
+            </div>
+
           </div>
-          <div class="w-full h-1/4 py-2 text-center">
-            <h2 class="font-rubik font-semibold text-darkgray text-[0.6rem]">TIMEZONE</h2>
-            <p class="font-rubik font-semibold text-blackdarkgray text-lg">
-              ${data.location.timezone}
-            </p>
+
+          <div class="w-full h-1/4 py-2 text-center lg:h-1/2 px-5 flex flex-col items-start border-r-2 border-darkgray-500">
+            <div>
+              <h2 class="font-rubik font-semibold text-darkgray text-[0.6rem]">TIMEZONE</h2>
+            </div>
+            <div>
+              <p class="font-rubik font-semibold text-blackdarkgray text-lg">
+                ${data.location.timezone}
+              </p>
+            </div>
           </div>
-          <div class="w-full h-1/4 py-2 text-center">
-            <h2 class="font-rubik font-semibold text-darkgray text-[0.6rem]">ISP</h2>
-            <p class="font-rubik font-semibold text-blackdarkgray text-lg">
-              ${data.isp}
-            </p>
+
+          <div class="w-full h-1/4 py-2 text-center lg:h-1/2 px-5 flex flex-col items-start">
+            <div>
+              <h2 class="font-rubik font-semibold text-darkgray text-[0.6rem]">ISP</h2>
+            </div>
+            <div>
+              <p class="font-rubik font-semibold text-blackdarkgray text-lg">
+                ${data.isp}
+              </p>
+            </div>
           </div>
         </div>
     `
